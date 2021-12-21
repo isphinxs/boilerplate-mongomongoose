378a88f8d42d79a20b300fd7ed802e6a77dc951e
Samantha Ostrowski <isphinxs@hotmail.com>
2021-12-20 19:37:21 -0500

    Add find and update

--- myApp.js
+++ myApp.js
@@ -62,8 +62,10 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, function(err, person) {
+    if (err) return done(err);
+    done(null, person);
+  });
 };
 
 const removeById = (personId, done) => {
